3a18cdf7c9ead81fbd8a6ed1820893f385e92f14
waelnekkach2012-beep <wael.nekkach2012@gmail.com>
2026-08-13 17:58:57 +0300

    Create Constants.java for configuration settings
    Add Constants class with server URL and heartbeat interval

---
+++ android-client/app/src/main/java/com/yourcompany/quasarmobile/Constants.java
@@ -0,0 +1,8 @@
+// Constants.java
+package com.yourcompany.quasarmobile;
+
+public class Constants {
+    // IMPORTANT: Remplacez par l'IP PUBLIQUE de votre Codespace et le port 3000
+    public static final String C2_SERVER_URL = "http://VOTRE_IP_PUBLIQUE_CODESPACE:3000";
+    public static final long HEARTBEAT_INTERVAL_MS = 30000; // 30 secondes
+}
